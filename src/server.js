319d182268084a54815bff831a231dd3cad19b5f
Felipe Fernandes <felipe.bravil@gmail.com>
2024-04-13 19:07:54 -0300

    feat: add server and entrypoint

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+//import cors from "cors";
+import { config } from "dotenv";
+
+import router from "./router.js";
+
+export default class Server {
+  constructor() {
+    this.app = express();
+  }
+
+  #configEnvironment() {
+    config();
+
+    this.port = process.env.SERVER_PORT ?? 3000;
+    this.hostname = process.env.SERVER_HOSTNAME ?? "localhost";
+  }
+
+  start() {
+    this.#configEnvironment();
+
+    this.app.use(express.json());
+    //this.app.use(cors());
+
+    this.app.use(router);
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on http://${this.hostname}:${this.port}`);
+    });
+  }
+}
